7cac1a04369bd19fedad13cc85b4070924e9d21b
ludavilla <30967356+ludavilla@users.noreply.github.com>
2021-10-08 11:13:22 -0300

    Delete main.tf

--- terraform/s3/main.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-  region = var.region
-  profile = var.profile
-}
-resource "aws_s3_bucket" "this" {
-  bucket = var.bucket
-  acl    = "private" ## private ou public
-  #tags = var.tags
-}
